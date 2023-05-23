094b509b95d4190a96c6507c132cc477bc696cfb
joshp27 <joshphipps2@aol.com>
2023-05-23 23:59:40 +0000

    This is my Code challenge Event Logging Tool Part 2 of 3

---
+++ code27.py
@@ -0,0 +1,54 @@
+#!/user/bin/python3
+# Script: Ops 401 Class 27 Code Challenge
+# Joshua Phipps
+# 5/23/2023
+# Purpose: Ops Challenge: Event Logging Tool Part 2 of 3
+
+
+import logging
+from logging.handlers import RotatingFileHandler
+from scapy.all import *
+
+# Configure logging with log rotation
+log_file = 'log.txt'
+max_log_size = 500  # 1 MB
+backup_count = 2  # Number of backup log files to keep
+
+# Create a rotating file handler for log rotation
+log_handler = RotatingFileHandler(log_file, maxBytes=max_log_size, backupCount=backup_count)
+log_handler.setLevel(logging.DEBUG)
+log_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
+
+# Create a logger and add the log handler
+logger = logging.getLogger()
+logger.addHandler(log_handler)
+
+# Define the host IP and port range to scan
+host = "scanme.nmap.org"
+port_range = [22, 23, 80, 443, 3389]
+
+# Define the TCP Port Range Scanner function
+def tcp_port_scanner(host, port):
+    try:
+        # Send SYN packet and wait for response
+        syn_packet = IP(dst=host) / TCP(dport=port, flags="S")
+        response = sr1(syn_packet, timeout=1, verbose=0)
+
+        # Check the response flags to determine if the port is open, closed, or filtered
+        if response is None:
+            logger.warning(f"Port {port} is filtered or host is down")
+        elif response.haslayer(TCP) and response.getlayer(TCP).flags == 0x12:
+            # Send RST packet to close the open connection
+            rst_packet = IP(dst=host) / TCP(dport=port, flags="AR")
+            send(rst_packet, verbose=0)
+            logger.info(f"Port {port} is open")
+        elif response.haslayer(TCP) and response.getlayer(TCP).flags == 0x14:
+            logger.info(f"Port {port} is closed")
+        else:
+            logger.warning(f"Port {port} is filtered or host is down")
+    except Exception as e:
+        logger.error(f"An error occurred while scanning port {port}: {str(e)}")
+
+# Iterate over the port range and call the TCP Port Range Scanner function for each port
+for port in port_range:
+    tcp_port_scanner(host, port)
